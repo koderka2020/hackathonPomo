5cfa207885adbd4edbe8e2a8ebcdc9fbe3681315
zbrucker <zach.brucker@gmail.com>
2021-02-04 10:46:36 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let startPomo = document.getElementById("startPomo");
+
+startPomo.addEventListener("click", () => {console.log('hello')});
+
+
+// function testFunc()=>{
+//   return setTimeout(()=>{alert("Your timer is on!")}, 5000);
+// }
+
+
+//REMOVE
+//const test = document.getElementById('top-nav')
+//test.parentNode.removeChild(test)
+
+
+//REPLACE WITH
+
+
